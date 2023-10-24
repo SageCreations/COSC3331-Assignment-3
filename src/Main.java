7e56ca8bc63529dddc294747b2cf6d3cdd8ba253
SageCreations <sagecreations.ec@gmail.com>
2023-10-24 11:18:40 -0500

    stuck on option 1 ;_;

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
             DisplayMenu();
             choice = reader.nextInt();
+            reader.nextLine();
 
             switch (choice) {
                 case 1:
@@ -40,14 +41,35 @@ public class Main {
     }
 
     public static void OptionOne(Scanner reader) {
+        // instantiation of the link lists needed for this option.
+        LinkList wordOne = new LinkList();
+        LinkList wordTwo = new LinkList();
 
+        // clarity for option chosen
         System.out.println("\n\nZig Zag Weave: ");
-        System.out.print("Please write the first word to be used :");
+
+        //get first word from user.
+        System.out.print("Please type the first word to be used: ");
         String stringOne = reader.nextLine();
+        // O(n) already ;_;
+        StringToLinkList(stringOne, wordOne);
 
 
+        // Get second word from user.
+        System.out.print("Please type the second word to be used: ");
         String stringTwo = reader.nextLine();
+        // another O(n) ;_;
+        StringToLinkList(stringTwo, wordTwo);
 
+        int index = 0;
+        // do the zig zag
+        Link current = wordOne.first;
+        boolean flip = true; // true because we already have word one on current
+        while (current != null) {
+            if (flip) {
+
+            }
+        }
     }
 
     public static void OptionTwo() {
@@ -56,5 +78,12 @@ public class Main {
 
     public static void OptionThree() {
 
+    }
+
+    // there's probably gonna be a lot of this....
+    public static void StringToLinkList(String wordString, LinkList wordLink) {
+        for (int i = wordString.length()-1; i >= 0 ; i--) {
+            wordLink.InsertFirst(i, wordString.charAt(i));
+        }
     }
 }
